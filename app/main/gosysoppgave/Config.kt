947af96fc9548cf4c8f653b3c45e4ab1fb19b06f
Niro Karunakaran <niro.karunakaran@nav.no>
2023-01-27 12:16:54 +0100

    initial commit

---
+++ app/main/gosysoppgave/Config.kt
@@ -0,0 +1,9 @@
+package gosysoppgave
+
+import no.nav.aap.kafka.streams.KStreamsConfig
+import no.nav.aap.ktor.client.AzureConfig
+
+internal data class Config(
+    val azure: AzureConfig,
+    val kafka: KStreamsConfig,
+)
---
+++ app/test/gosysoppgave/AppTest.kt
@@ -0,0 +1,13 @@
+package gosysoppgave
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+
+class AppTest {
+    @Test
+    fun test(){
+        assertTrue(true)
+    }
+
+}
